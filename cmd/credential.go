242bd2b0142762a9ae72c6f36d0eced9a7c582b2
YoshinoriN <nayo-msoft@outlook.jp>
2020-11-08 21:34:23 +0900

    feat: 'credential list' command

--- cmd/credential.go
+++ cmd/credential.go
@@ -26,15 +26,58 @@ var credentialCommand = &cobra.Command{
 	},
 }
 
+// TODO: refactor
 var showCredentialCommmand = &cobra.Command{
 	Use:   "list",
 	Short: "Show all of cahsper credential variables.",
 	Long:  "Show all of cahsper credential variables.",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: check file is already exists or not
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		fmt.Print("UserName: ")
+		scanner.Scan()
+		userName := scanner.Text()
+		if userName == "" {
+			fmt.Print("UserName required.")
+			os.Exit(0)
+		}
+
+		var err error
+		var password = ""
+		userName, password, err = utils.GetAccount(userName)
+		if err != nil {
+			if strings.Contains(fmt.Sprintln(err), "secret not found in keyring") {
+				fmt.Printf("UserName %s does not exists.\n", userName)
+				os.Exit(0)
+			} else {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+		}
+		fmt.Println("Password: ", password)
+
+		idToken, err := utils.GetCredential(userName, utils.IDToken)
+		if err != nil {
+			if !strings.Contains(fmt.Sprintln(err), "secret not found in keyring") {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+		}
+		fmt.Println("IdToken: ", idToken)
+
+		accessToken, err := utils.GetCredential(userName, utils.AccessToken)
+		if err != nil {
+			if !strings.Contains(fmt.Sprintln(err), "secret not found in keyring") {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+		}
+		fmt.Println("AccessToken: ", accessToken)
 	},
 }
 
+// TODO: refactor
 var setCredentialCommand = &cobra.Command{
 	Use:   "set",
 	Short: "Set cahsper credential variables.",
